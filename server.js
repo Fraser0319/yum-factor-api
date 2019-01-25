02c5d03afbd5a311a6c517ca12167342c14bae5e
Fraser Beaton <fraser0319@gmail.com>
2019-01-25 16:24:58 +0000

    removed some packages i dont need.

--- server.js
+++ server.js
@@ -3,16 +3,10 @@ var bodyParser = require('body-parser');
 const api = require('./routes/api');
 const app = express();
 const port = 3000;
-const cors = require('cors');
-const morgan = require('morgan');
 const mongoose = require('mongoose');
-const compression = require('compression');
 require('dotenv').config();
 
-app.use(morgan('combined'));
 app.use(bodyParser.json());
-app.use(cors());
-app.use(compression());
 app.use('/api/v1', api);
 
 mongoose.connect(process.env.MONGO_CONNECT);
