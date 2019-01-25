f8eec0e23d9be876b0e7400356c626463bf01c96
Fraser Beaton <fraser0319@gmail.com>
2019-01-25 12:56:40 +0000

    installed dotenv and can now connect to mlab through mongoose.

--- server.js
+++ server.js
@@ -1,18 +1,26 @@
 const express = require('express');
 var bodyParser = require('body-parser');
-const api = require("./routes/api");
+const api = require('./routes/api');
 const app = express();
 const port = 3000;
-const cors = require("cors");
-const morgan = require("morgan");
-const mongoose = require("mongoose");
-const compression = require("compression");
+const cors = require('cors');
+const morgan = require('morgan');
+const mongoose = require('mongoose');
+const compression = require('compression');
+require('dotenv').config();
 
-
-app.use(morgan("combined"));
+app.use(morgan('combined'));
 app.use(bodyParser.json());
 app.use(cors());
 app.use(compression());
-app.use("/api/v1", api);
+app.use('/api/v1', api);
+
+mongoose.connect(process.env.MONGO_CONNECT);
+
+var db = mongoose.connection;
+db.on('error', console.error.bind('Connection Error'));
+db.once('open', function() {
+  console.log('Connected Successfully');
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
